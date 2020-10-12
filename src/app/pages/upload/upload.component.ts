3772025b6d3b5d91c1cc893406a995fb574b5680
Tran Minh Hieu <alwaysbelieve239@gmail.com>
2020-10-12 10:25:23 +0700

    Reimplement upload page

--- src/app/pages/upload/upload.component.ts
+++ src/app/pages/upload/upload.component.ts
@@ -1,10 +1,6 @@
 import { Component, TemplateRef, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
 import { NzNotificationService } from 'ng-zorro-antd/notification';
-import { ImageCroppedEvent } from 'ngx-image-cropper';
-import { Coordinate, Region } from 'src/app/models/text-region';
-import { BackendService } from 'src/app/services/backend.service';
-import { ThumbnailService } from 'src/app/services/thumbnail.service';
+import { NzUploadChangeParam } from 'ng-zorro-antd/upload';
 
 @Component({
   selector: 'app-upload',
@@ -14,100 +10,26 @@ import { ThumbnailService } from 'src/app/services/thumbnail.service';
 export class UploadComponent {
   @ViewChild('imageNotification', { static: false }) imageNotificationTemplate: TemplateRef<{}>;
 
-  public displayTools: boolean;
-  public imageChangedEvent: any;
-  public croppedRegions: Region[];
-  public croppedRegionImages: string[];
-  public croppedRegionThumbnails: string[];
-  public currentRegion: Region;
-  public currentRegionImage: string;
-
   constructor(
-    private thumbnail: ThumbnailService,
-    private backend: BackendService,
-    private router: Router,
     private notification: NzNotificationService
-  ) {
-    this.initialize();
-  }
+  ) { }
 
-  initialize() {
-    this.displayTools = false;
-    this.imageChangedEvent = null;
-    this.croppedRegions = [];
-    this.croppedRegionImages = [];
-    this.croppedRegionThumbnails = [];
-    this.currentRegion = null;
-    this.currentRegionImage = null;
-  }
-
-  fileChangeEvent(event: any): void {
-    this.initialize();
-    this.imageChangedEvent = event;
-  }
-
-  imageCropped(event: ImageCroppedEvent) {
-    const x1: number = event.cropperPosition.x1;
-    const x2: number = event.cropperPosition.x2;
-    const y1: number = event.cropperPosition.y1;
-    const y2: number = event.cropperPosition.y2;
-    this.currentRegion = new Region([
-      new Coordinate(x1, y1),
-      new Coordinate(x1, y2),
-      new Coordinate(x2, y2),
-      new Coordinate(x2, y1)
-    ]);
-    this.currentRegionImage = event.base64;
-  }
-
-  imageLoaded() {
-  }
-
-  cropperReady() {
-    this.displayTools = true;
-  }
-
-  loadImageFailed() {
-    this.notification.error('Failed to load file', 'The selected file may be of incorrect format or corrupted');
-  }
-
-  addSelected() {
-    this.thumbnail.generateThumbnail(this.currentRegionImage, 160, 90).then((thumbnail) => {
-      this.croppedRegions.push(this.currentRegion);
-      this.croppedRegionImages.push(this.currentRegionImage);
-      this.croppedRegionThumbnails.push(thumbnail);
-    });
-  }
-
-  resetRegions() {
-    this.croppedRegions = [];
-  }
-
-  deleteRegion(id: number) {
-    this.croppedRegions.splice(id, 1);
-    this.croppedRegionImages.splice(id, 1);
-    this.croppedRegionThumbnails.splice(id, 1);
-  }
-
-  upload() {
-    const imageFile: File = this.imageChangedEvent.srcElement.files[0];
-    this.backend.uploadImage('123', imageFile, this.croppedRegions)
-      .then(() => {
-        const reader: FileReader = new FileReader();
-        reader.onloadend = () => {
-          this.notification.template(this.imageNotificationTemplate, {
-            nzDuration: 6000,
-            nzData: {
-              title: 'File uploaded sucessfully',
-              description: 'Text regions will be automatically detected. You can edit them via My Images page.',
-              imgSrc: reader.result
-            }
-          });
-          this.router.navigateByUrl('/');
-        };
-        reader.readAsDataURL(imageFile);
-      }, (reason) => {
-        this.notification.error('Failed to upload file', `Reason: ${reason}`);
-      });
+  handleChange({ file }: NzUploadChangeParam): void {
+    const status = file.status;
+    if (status === 'done') {
+      const reader: FileReader = new FileReader();
+      reader.onloadend = () => {
+        this.notification.template(this.imageNotificationTemplate, {
+          nzDuration: 6000,
+          nzData: {
+            title: 'File uploaded sucessfully',
+            description: 'Text regions will be automatically detected. You can edit them via My Images page.',
+            imgSrc: file.thumbUrl
+          }
+        });
+      };
+    } else if (status === 'error') {
+      this.notification.error('Failed to upload file', '');
+    }
   }
 }
--- src/app/pages/upload/upload.module.ts
+++ src/app/pages/upload/upload.module.ts
@@ -2,36 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UploadComponent } from './upload.component';
 import { UploadRoutingModule } from './upload-routing.module';
-import { NzInputModule } from 'ng-zorro-antd/input';
-import { NzGridModule } from 'ng-zorro-antd/grid';
-import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzIconModule, NZ_ICONS } from 'ng-zorro-antd/icon';
-import { NzCardModule } from 'ng-zorro-antd/card';
-import { NzPopoverModule } from 'ng-zorro-antd/popover';
-import { ImageCropperModule } from 'ngx-image-cropper';
-import { FormsModule } from '@angular/forms';
 import { NzTypographyModule } from 'ng-zorro-antd/typography';
 import { NzNotificationModule } from 'ng-zorro-antd/notification';
+import { NzUploadModule } from 'ng-zorro-antd/upload';
 import { IconDefinition } from '@ant-design/icons-angular';
-import { PlusOutline, DeleteOutline, UploadOutline } from '@ant-design/icons-angular/icons';
+import { InboxOutline } from '@ant-design/icons-angular/icons';
 
-const icons: IconDefinition[] = [PlusOutline, DeleteOutline, UploadOutline];
+const icons: IconDefinition[] = [InboxOutline];
 
 @NgModule({
   declarations: [UploadComponent],
   imports: [
     CommonModule,
     UploadRoutingModule,
-    NzInputModule,
-    NzGridModule,
-    NzButtonModule,
     NzIconModule,
-    NzCardModule,
-    NzPopoverModule,
     NzTypographyModule,
     NzNotificationModule,
-    ImageCropperModule,
-    FormsModule
+    NzUploadModule,
   ],
   exports: [
     UploadComponent
