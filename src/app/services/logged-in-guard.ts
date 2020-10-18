0c97808efbc1505d998751742e419f8e8bc34c26
Tran Minh Hieu <alwaysbelieve239@gmail.com>
2020-10-18 15:10:33 +0700

    Improve authentication check on logged-in-guard

--- src/app/services/logged-in-guard.ts
+++ src/app/services/logged-in-guard.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, UrlTree } from '@angular/router';
+import User from '../models/user';
 import { AuthService } from './auth.service';
 
 @Injectable()
@@ -9,17 +10,42 @@ class UserLoggedInGuard implements CanActivate {
         private router: Router
     ) { }
 
-    canActivate(): Promise<boolean> {
-        return new Promise<boolean>((resolve, reject) => {
-            this.auth.isUserLoggedIn().then((result) => {
-                if (result) {
-                    resolve(true);
-                } else {
-                    this.router.navigateByUrl('/login');
-                    resolve(false);
+    canActivate(
+        route: ActivatedRouteSnapshot
+    ): Promise<boolean | UrlTree> {
+        return new Promise<boolean | UrlTree>((resolve, reject) => {
+            this.auth.getCurrentUser().then((user) => {
+                if (!user) {
+                    return resolve(this.router.parseUrl('/login'));
                 }
+                if (!this.isUserAuthorized(route, user)) {
+                    return resolve(this.router.parseUrl('/welcome'));
+                }
+                return resolve(true);
             }, reject);
         });
+    }
+
+    private isUserAuthorized(route: ActivatedRouteSnapshot, user: User): boolean {
+        if (route.url.length == 0) {
+            return true;
+        }
+        switch (route.url[0].path) {
+            case 'upload':
+            case 'manage-image':
+            case 'my-images':
+                return user.canUpload;
+            case 'label':
+                return user.canLabel;
+            case 'verify':
+                return user.canVerify;
+            case 'export':
+                return user.canExport;
+            case 'manage-users':
+                return user.canManageUsers;
+            default:
+                return true;
+        }
     }
 }
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  craftRequest: 'http://localhost:8000/api/get-region',
+  exportRequest: 'http://localhost:8080/api/export',
+  exportStatusRequest: 'http://localhost:8080/api/export-status',
+  exportDownloadRequest: 'http://localhost:8080/api/download'
 };
